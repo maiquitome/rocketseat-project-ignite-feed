9eb7a67d6c98f9858f1797e930d201111d08bbfe
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-09-30 08:54:06 -0300

    feat: :sparkles: add properties to Post

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,8 @@ import { Post } from "./Post";
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post author="Maiqui Tomé" content="Conteúdo do post do Maiqui" />
+      <Post author="Diego Fernandes" content="Conteúdo do post do Diego" />
     </div>
   );
 }
