4ee6335fd41f6b462ab736bb8eda1c185cf1b0ae
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-09-30 23:11:01 -0300

    refactor: :recycle: change cover image

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,15 @@
 import styles from "./Sidebar.module.css";
 
-import coverImage from "../assets/cover.jpg";
+// import coverImage from "../assets/cover.jpg";
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      {/* <img src="https://images.unsplash.com/photo-1525498128493-380d1990a112?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" /> */}
-      <img className={styles.cover} src={coverImage} />
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      {/* <img className={styles.cover} src={coverImage} /> */}
 
       <div className={styles.profile}>
         <strong>Maiqui Tomé</strong>
