0bd648f5acdcb80946e86aeb81f490fc5b81183f
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-10-02 15:25:22 -0300

    feat: :sparkles: add Comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import styles from "./Comment.module.css";
+import { ThumbsUp, Trash } from "phosphor-react";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/maiquitome.png" alt="avatar" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Maiqui Tomé</strong>
+              <time title="11 de Maio às 08:13" dateTime="2022-05-11 08:13:30">
+                Há cerca de 2h
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
